90f18bbd5a71a3437c7fcfa5824a2f7ba90ca3f7
Boldinova Natalya <boldinova_natalya@mail.ru>
2015-04-01 17:05:27 +0300

    add average transition time to Statistics

--- src/main/scala/ru/vsu/cs/boldinovanatalya/trafficcontrol/utils/Statistics.scala
+++ src/main/scala/ru/vsu/cs/boldinovanatalya/trafficcontrol/utils/Statistics.scala
@@ -4,12 +4,14 @@ package ru.vsu.cs.boldinovanatalya.trafficcontrol.utils
 import ru.vsu.cs.boldinovanatalya.trafficcontrol.geneticalgorithm.TrainingElement
 import ru.vsu.cs.traffic.Color.{YELLOW, RED, GREEN}
 import ru.vsu.cs.traffic.Direction.FORWARD
-import ru.vsu.cs.traffic.event.{ModelActed, ColorChanged, BeforeColorChanged, VehicleSpawned}
-import ru.vsu.cs.traffic.{Color, Point, TrafficModel}
+import ru.vsu.cs.traffic.event._
+import ru.vsu.cs.traffic.{Vehicle, Color, Point, TrafficModel}
 
+import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.Map
 
-class Statistics(model: TrafficModel, interval: Double = 10) {
+class Statistics(model: TrafficModel, interval: Double = 1) {
   private val MinSpeed = 3
 
   private var approachingGreen_ = 0.0
@@ -18,19 +20,25 @@ class Statistics(model: TrafficModel, interval: Double = 10) {
   private var _currentQueuing = ListBuffer[(Double, Double)]()
   private var _lastTime = 0.0
 
+  private var _spawnedTime = mutable.Map[Vehicle, Double]()
+  private var _transitionTime = mutable.Map[Vehicle, Double]()
+
   def maxQueuing = _maxQueuing
-
   def maxApproaching = _maxApproaching
-
   def currentQueuing = _currentQueuing.toList
   def averageQueuing = _currentQueuing.map(_._2).sum / _currentQueuing.length
+  def averageTransitionTime = _transitionTime.map(_._2).sum / _transitionTime.size
 
 
   model.vehicleEventHandlers += {
     case VehicleSpawned(vehicle) =>
+      _spawnedTime += vehicle -> model.currentTime
       if (model.trafficLights.filter(_.color == GREEN).map(_(FORWARD)).contains(vehicle.trafficFlow)) {
         approachingGreen_ += 1
       }
+    case VehicleRemoved(vehicle) =>
+      _transitionTime += vehicle -> (model.currentTime - _spawnedTime(vehicle))
+
     case _ => Unit
   }
 
